03dd313c45f68afeb6ba32223779c08d720962b2
naveen <kmnaveen2410@gmail.com>
2026-07-26 22:46:00 +0530

    fix: cookies error

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const COOKIE_OPTS = {
   httpOnly: true,
-  secure: isProd,
-  sameSite: isProd ? 'none' : 'lax',
+  secure: true,
+  sameSite: 'None',
   path: '/',
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
